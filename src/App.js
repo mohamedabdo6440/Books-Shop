41e58f4694f85dd5b52c44d202620b61f51640a0
Mohamed Abdo <102926241+mohamedabdo6440@users.noreply.github.com>
2022-08-25 15:49:42 +0200

    add changed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 
 import React from 'react';
 import BodyApp from './component/BodyApp/BodyApp';
-import data from './data.json'
+
+
 
 function App() {
 
   return (
     <div>
       <BodyApp />
-
       {
-        data.results.map(d => {
 
-          return (
-            <div>
-
-            </div>
-          )
-
-        })
       }
     </div>
   );
